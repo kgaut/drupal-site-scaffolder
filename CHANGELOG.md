17cfc1e57a05939f8a13c42737a6652489e0bd97
Kevin Gautreau <contact@kgaut.net>
2015-12-15 18:42:59 +0100

    Post install script - copy the services.yml file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 ## [Unreleased]
 ### Changed
 - Preparing Drupal 8 version
+- Post install script, copy the services.yml file
--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -8,6 +8,14 @@ if [ ! -f www/sites/default/settings.php ]
     echo "default.settings.php copied to settings.php"
 fi
 
+# Prepare the services file for installation
+if [ ! -f www/sites/default/services.yml ]
+  then
+    cp www/sites/default/default.services.yml www/sites/default/services.yml
+    chmod 777 www/sites/default/services.yml
+    echo "default.services.yml copied to services.yml"
+fi
+
 # Prepare the files directory for installation
 if [ ! -d www/sites/default/files ]
   then
